f0cd56dfc935e47abc8b483f4ea1e064b2f18331
whitzhu <whitzhu@gmail.com>
2017-04-01 23:15:58 -0700

    react-router navbar

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Login from './components/Login.jsx';
 import Home from './components/Home.jsx';
 import CreateDecks from './components/CreateDecks.jsx';
 import AllDecks from './components/AllDecks.jsx';
+import DeckProfile from './components/DeckProfile.jsx';
 import Summary from './components/Summary.jsx';
 import Profile from './components/Profile.jsx';
 import DeckView from './components/DeckView.jsx';
@@ -16,17 +18,24 @@ class App extends Component {
 
   render () {
     return (
-    <div>
-      <Navbar />
-      <Login />
-      <Home />
-      <CreateDecks />
-      <AllDecks />
-      <DeckView />
-      <Summary />
-      <Profile />
+      <div>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path='/' component={Home}/>
+            <Route path='/all-decks' component={AllDecks}/>
+            <Route path='/creat-decks' component={CreateDecks}/>
+            <Route path='/deck-profile' component={DeckProfile}/>
+            <Route path='/deck-view' component={DeckView}/>
+            <Route path='/profile' component={Profile}/>
+            <Route path='/summary' component={Summary}/>
+            <Route path='/login' component={Login}/>
+          </div>
+        </Router>
     </div>)
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
